6d5439df59b1733d33cda6f44d9b11aa8b60c878
Jean-Baptiste Desbas <jb.desbas@gmail.com>
2019-07-23 15:34:09 +0200

    frontend map

--- frontend/app/components/validation-info-obs/validation-info-obs.component.ts
+++ frontend/app/components/validation-info-obs/validation-info-obs.component.ts
@@ -10,14 +10,19 @@ export class ValidationInfoObsComponent implements OnInit {
 
   @Input() cd_nom: number;
   properties: any[];
+  data:any[];
 
   constructor(private dataService: DataService) {}
-
+  
   ngOnInit() {
 
      this.dataService.getOneSyntheseObservation(this.cd_nom).subscribe(
         data => {
             this.properties=data['properties'];
+            this.data=data;
+            this.latLng=[data.geometry.coordinates[1],data.geometry.coordinates[0]]; //ajouter un trick pour gerer les lignes, polygones, multipoint, etc.. --> Turfjs ?
+            this.geoJson={type:'FeatureCollection', features:[data]};
+          
         }
 
     )
