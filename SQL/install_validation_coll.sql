2fa563c8da9e95ca1f397946459a2adf7ef906df
Jean-Baptiste Desbas <jb.desbas@gmail.com>
2019-08-06 10:41:10 +0200

    script sql install

--- SQL/install_validation_coll.sql
+++ SQL/install_validation_coll.sql
@@ -1,9 +1,9 @@
---TODO ajouter les commentaires de validation
+BEGIN;
 
-CREATE SCHEMA gn_module_validation_col;
+CREATE SCHEMA gn_module_validation_obo;
 
 ---DEV----
-        CREATE TABLE gn_module_validation_col.t_validation_test (
+        CREATE TABLE gn_module_validation_obo.t_validation_test (
 	        id_validation serial PRIMARY KEY,
 	        uuid_attached_row uuid NULL,
 	        id_nomenclature_valid_status int4 NULL,
@@ -14,7 +14,7 @@ CREATE SCHEMA gn_module_validation_col;
         );
 -------
 
-CREATE TABLE gn_module_validation_col.t_vote_validation (
+CREATE TABLE gn_module_validation_obo.t_vote_validation (
     id_vote_validation serial PRIMARY KEY,
     uuid_attached_row uuid,
     id_validator int,
@@ -23,32 +23,32 @@ CREATE TABLE gn_module_validation_col.t_vote_validation (
     commentaire text
 );
 
-CREATE TABLE gn_module_validation_col.cor_vote_validation (
-    id_vote_validation integer REFERENCES gn_module_validation_col.t_vote_validation(id_vote_validation),
-    id_validation integer REFERENCES gn_module_validation_col.t_validation_test(id_validation) --A REMPLACER PAR LA TABLE gn_commons.validations
+CREATE TABLE gn_module_validation_obo.cor_vote_validation (
+    id_vote_validation integer REFERENCES gn_module_validation_obo.t_vote_validation(id_vote_validation),
+    id_validation integer REFERENCES gn_module_validation_obo.t_validation_test(id_validation) --A REMPLACER PAR LA TABLE gn_commons.validations
 );
 
-ALTER TABLE gn_module_validation_col.t_vote_validation ADD CONSTRAINT check_t_validations_valid_status 
+ALTER TABLE gn_module_validation_obo.t_vote_validation ADD CONSTRAINT check_t_validations_valid_status 
     CHECK (ref_nomenclatures.check_nomenclature_type_by_mnemonique(id_nomenclature_valid_status, 'STATUT_VALID'::character varying)) NOT VALID;
 
 
 --TRIGGER
-CREATE OR REPLACE FUNCTION gn_module_validation_col.trg_insert_validation_vote()
+CREATE OR REPLACE FUNCTION gn_module_validation_obo.trg_insert_validation_vote()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
  DECLARE n_vote integer;
  DECLARE max_vote_same_statut integer;   
 BEGIN
- SELECT INTO max_vote_same_statut max("count") FROM ( SELECT count(*) AS "count" FROM gn_module_validation_col.t_vote_validation WHERE uuid_attached_row=NEW.uuid_attached_row
+ SELECT INTO max_vote_same_statut max("count") FROM ( SELECT count(*) AS "count" FROM gn_module_validation_obo.t_vote_validation WHERE uuid_attached_row=NEW.uuid_attached_row
  GROUP BY id_nomenclature_valid_status) AS a;
  IF (max_vote_same_statut >= 2 ) THEN --Vote fermées
     RAISE EXCEPTION 'Les votes sont fermés';
     RETURN NULL;
  END IF;
- SELECT INTO n_vote count(*) FROM gn_module_validation_col.t_vote_validation WHERE uuid_attached_row=NEW.uuid_attached_row AND id_nomenclature_valid_status=NEW.id_nomenclature_valid_status;
+ SELECT INTO n_vote count(*) FROM gn_module_validation_obo.t_vote_validation WHERE uuid_attached_row=NEW.uuid_attached_row AND id_nomenclature_valid_status=NEW.id_nomenclature_valid_status;
  IF (n_vote = 1 ) THEN --Le vote en cours et le seconde
-    INSERT INTO gn_module_validation_col.t_validation_test ( uuid_attached_row, id_nomenclature_valid_status, validation_auto, validation_comment, validation_date ) --A REMPLACER PAR LA TABLE gn_commons.validations
+    INSERT INTO gn_module_validation_obo.t_validation_test ( uuid_attached_row, id_nomenclature_valid_status, validation_auto, validation_comment, validation_date ) --A REMPLACER PAR LA TABLE gn_commons.validations
     VALUES (NEW.uuid_attached_row, NEW.id_nomenclature_valid_status, false, 'validation collegiale', now() );
     RETURN NEW;
  END IF;
@@ -59,10 +59,10 @@ END;
 $function$
 ;
 
-COMMENT ON FUNCTION gn_module_validation_col.trg_insert_validation_vote() IS 'Attribuer un statut si 2 validateurs ont voté pour le même statut';
+COMMENT ON FUNCTION gn_module_validation_obo.trg_insert_validation_vote() IS 'Attribuer un statut si 2 validateurs ont voté pour le même statut';
 
-CREATE TRIGGER tri_insert_validation_vote BEFORE INSERT ON gn_module_validation_col.t_vote_validation
-    FOR EACH ROW EXECUTE PROCEDURE gn_module_validation_col.trg_insert_validation_vote();
+CREATE TRIGGER tri_insert_validation_vote BEFORE INSERT ON gn_module_validation_obo.t_vote_validation
+    FOR EACH ROW EXECUTE PROCEDURE gn_module_validation_obo.trg_insert_validation_vote();
 
 --TODO prevoir suppression/update de vote ?
 
@@ -70,11 +70,24 @@ CREATE TRIGGER tri_insert_validation_vote BEFORE INSERT ON gn_module_validation_
 
 
 --Priorisation de la validation
-CREATE TABLE gn_module_validation_col.t_validation_priority(
+CREATE TABLE gn_module_validation_obo.t_validation_priority(
     uuid_attached_row uuid PRIMARY KEY,
     priority integer NOT NULL
 );
-COMMENT ON TABLE gn_module_validation_col.t_validation_priority IS 'Cette table permet de soumettre manuellement certaines données en validation prioritaire';
+COMMENT ON TABLE gn_module_validation_obo.t_validation_priority IS 'Cette table permet de soumettre manuellement certaines données en validation prioritaire';
+
+--WIP initiatlisation de la liste de familles et ordres
+
+INSERT INTO taxonomie.bib_listes (id_liste, nom_liste, desc_liste)
+VALUES (600, 'Validation OneByOne','Taxons pouvant être choisie pour lancer la validation, généralement il s''agit d''ordre ou de familles');--TODO prendre l'id dans les parametres
+
+INSERT INTO taxonomie.cor_nom_liste (id_nom, id_liste)
+SELECT id_nom, 600 ----TODO prendre l'id_liste dans les parametres
+FROM taxonomie.bib_noms bib_noms
+JOIN taxonomie.taxref taxref ON bib_noms.cd_nom = taxref.cd_nom
+WHERE taxref.id_rang IN ('CL','OR','FM');
+
+COMMIT;
 
 
 
