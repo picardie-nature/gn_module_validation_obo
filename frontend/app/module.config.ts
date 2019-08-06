3aee3cfcb239e6afd7ac465be96292fbb78b8944
Jean-Baptiste Desbas <jb.desbas@gmail.com>
2019-08-06 10:39:18 +0200

    changement nom module

--- frontend/app/module.config.ts
+++ frontend/app/module.config.ts
@@ -1,7 +1,7 @@
 export const ModuleConfig = {
  "ID_LISTE_TAXONS": 600,
  "ID_MODULE": 10,
- "MODULE_CODE": "VALIDATION_COL",
- "MODULE_URL": "validation_col",
+ "MODULE_CODE": "VALIDATION_OBO",
+ "MODULE_URL": "validation_obo",
  "N_RANDOM": 5
 }
